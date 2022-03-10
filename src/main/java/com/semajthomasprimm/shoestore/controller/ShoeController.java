3c197a62f29c08d38e669ee0e5a03dfaea653289
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2022-03-10 17:19:49 -0500

    put ShoesController in a subfolder controller
    Signed-off-by: Semaj Primm <semajthomasprimm@users.noreply.github.com>

--- src/main/java/com/semajthomasprimm/shoestore/controller/ShoeController.java
+++ src/main/java/com/semajthomasprimm/shoestore/controller/ShoeController.java
@@ -1,4 +1,4 @@
-package com.semajthomasprimm.shoestore;
+package com.semajthomasprimm.shoestore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
