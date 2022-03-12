8132c11bd435e51c0ec39fa32c6ae6d75429a13a
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2022-03-11 20:06:03 -0500

    added getters/setters to ProductEntity
    Signed-off-by: Semaj Primm <semajthomasprimm@users.noreply.github.com>

--- src/main/java/com/semajthomasprimm/shoestore/database/ProductEntity.java
+++ src/main/java/com/semajthomasprimm/shoestore/database/ProductEntity.java
@@ -26,4 +26,51 @@ public class ProductEntity {
     @Column(name="description")
     private String description;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(String sizes) {
+        this.sizes = sizes;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
